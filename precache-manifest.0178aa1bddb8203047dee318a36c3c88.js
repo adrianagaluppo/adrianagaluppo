51d5ab8a834312f250f6e56f9dc8161e25be987e
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2019-03-29 08:47:53 -0300

    Updates

---
+++ precache-manifest.0178aa1bddb8203047dee318a36c3c88.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "57c5fd154132abd30cb9",
+    "url": "/adrianagaluppo/./static/js/runtime~main.8a9a28d8.js"
+  },
+  {
+    "revision": "3a99521141adea0c207e",
+    "url": "/adrianagaluppo/./static/js/main.b5face90.chunk.js"
+  },
+  {
+    "revision": "dfb8af626a003b093a80",
+    "url": "/adrianagaluppo/./static/js/2.9ac7ff98.chunk.js"
+  },
+  {
+    "revision": "f3ae24c49f593d4f80ee1db84eae5a1d",
+    "url": "/adrianagaluppo/./index.html"
+  }
+];
