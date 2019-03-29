250ad22a6236acfd431431db94f77e4461b175f5
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2019-03-29 08:48:52 -0300

    fix: Adjustments

--- src/utils/consts/contentfulKeys.js
+++ src/utils/consts/contentfulKeys.js
@@ -1,2 +1,2 @@
-export const space = 'XXXXX';
-export const accessToken = 'XXXXXX';
+export const space = 'd8j81amy65r8';
+export const accessToken = '767b090724c69240660d0418e0be6605fcc0899d91cf29758245747b7687d2df';
